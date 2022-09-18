2bcffb61c220b000741e3b5ae757e71d234eecbc
sajalrasto <30717607+sajalrasto@users.noreply.github.com>
2022-09-18 16:35:49 +0530

    Update PaymentService.js

--- evento-master/product-webapp/evanto-app/src/Services/PaymentService.js
+++ evento-master/product-webapp/evanto-app/src/Services/PaymentService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// let baseURL = "http://localhost:8080";
-let baseURL = "https://evento.stackroute.io";
+ let baseURL = "http://localhost:8080";
+ //let baseURL = "https://evento.stackroute.io";
 
 const headers = {
     'Content-Type': 'application/json',
